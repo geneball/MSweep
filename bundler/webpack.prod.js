e65b326d697c2152b930ad4953fa7bab94db5f4b
Gene Ball <geneball@gmail.com>
2023-05-20 14:51:53 -0400

    silv

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,101 @@
 const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = merge(
-    commonConfiguration,
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, '../src/app.js'),
+    output:
     {
-        mode: 'production',
-        plugins:
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    devtool: 'source-map',
+    plugins:
+    [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, '../static') }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+			title: 'Progressive Web App',
+            template: path.resolve(__dirname, '../src/index.html'),
+            minify: true
+        }),
+        new MiniCSSExtractPlugin(),
+		new WorkboxPlugin.GenerateSW({
+		//	these options encourage the ServiceWorkers to get in there fast
+		//	and not allow any straggling "old" SWs to hang around
+			clientsClaim: true,
+			skipWaiting: true,
+		}),
+    ],
+    module:
+    {
+        rules:
         [
-            new CleanWebpackPlugin()
+            // HTML
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+
+            // CSS
+            {
+                test: /\.css$/,
+                use:
+                [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+
+            // Images
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/images/'
+                        }
+                    }
+                ]
+            },
+
+            // Fonts
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'assets/fonts/'
+                        }
+                    }
+                ]
+            }
         ]
     }
-)
+}
+
